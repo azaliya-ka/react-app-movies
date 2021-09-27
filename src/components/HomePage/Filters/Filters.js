9fe347849f9ae96e61731298cf13ce004e66b1f6
Azaliia Kasimova <sea-blue@inbox.ru>
2021-09-27 18:20:38 +0300

    filter tabs

--- src/components/HomePage/Filters/Filters.js
+++ src/components/HomePage/Filters/Filters.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Select } from './../../../utils'
 import { movieFilters, sortMovies } from './../../../constants'
+import cn from 'classnames'
 import './Filters.css'
 
 const Filters = () => {
+  const [checkedTab, setCheckedTab] = useState('all');
+
     return (
-      <div className='filters'>
-        <div className='filters__text'>
-          {movieFilters.map(value => {
-            return (
-              <div className='filters__item' key={value}>{value}</div>
-            )
-          })}
+      <>
+        <div className='filters'>
+          <div className='filters__tabs'>
+            {movieFilters.map(value => {
+              return (
+                <div
+                  className={cn('filters__item', {['filters__checked']: checkedTab === value})}
+                  key={value}
+                  onClick={() => setCheckedTab(value)}
+                >
+                  {value}
+                </div>
+              )
+            })}
+          </div>
+          <div>
+            <span className='filters__sort'>Sort by</span>
+            <Select options={sortMovies} selectClass='filters__sortSelect' />
+          </div>
         </div>
-        <div>
-          <span className='filters__sort'>Sort by</span>
-          <Select options={sortMovies} selectClass='filters__sortSelect' />
-        </div>
-      </div>
+        <div className='filter__border' />
+      </>
     );
 }
 
